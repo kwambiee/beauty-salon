f08401493b13146815e5dd9f0db9c3cc44246376
Joy Kwamboka <joy@nathandigital.com>
2025-01-15 15:22:39 +0300

    add utility functions, configure image patterns, and enhance Tailwind setup

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // configure images
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.usegalileo.ai',
+        port: '',
+        pathname: '/sdxl10/**',
+        search: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
